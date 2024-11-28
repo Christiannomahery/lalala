0631d015474dbb50bb02862d30ce36f34aa3a8f6
christiano mahery <christianomahery@gmail.com>
2024-11-28 12:37:38 +0300

    Optimisé le code

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var n = parseInt (prompt ("Entrez le nombre: "))
-alert(n);
+var n = parseInt (prompt ("Entrez le nombre: "));
 for (let i=1; i<=n; i++) {
     for (let j=1; j<=n-i; j++){
         document.write(" ");
     }
     for (let k=1; k<=(2*i-1); k++){
-        document.write("*");
+        document.write(" *");
     }
     document.write(" "+"<br>");
   }
