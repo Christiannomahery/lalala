82178e82121c052333a20258a8e756e38b8881df
christiano mahery <christianomahery@gmail.com>
2024-11-28 12:31:51 +0300

    Devoir

---
+++ app.js
@@ -0,0 +1,11 @@
+var n = parseInt (prompt ("Entrez le nombre: "))
+alert(n);
+for (let i=1; i<=n; i++) {
+    for (let j=1; j<=n-i; j++){
+        document.write(" ");
+    }
+    for (let k=1; k<=(2*i-1); k++){
+        document.write("*");
+    }
+    document.write(" "+"<br>");
+  }
